d420def5ed7d3f0bb10de85408f75f296535d66f
Matthew Camus <sc21mc@leeds.ac.uk>
2024-01-31 17:23:59 +0000

    I have made progress in the first sprint. I am almost done making a series of functions that will allow for the reading of a corpus of XML files to be stored in a single list

--- Main.c
+++ Main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <dirent.h>
 
 //list of functions:
 int mainMenu();
@@ -8,6 +10,7 @@ int GenerateDataset();
 
 //dataset generation functions
 int LWD_Dataset();
+int wordReader(char fileLocation[128], FILE* Wfp);
 
 //main program to deal with what the user wishes to do with the system
 int main()
@@ -168,12 +171,118 @@ int LWD_Dataset()
     //clear the screen so that only relevant system text is shown
     system("cls");
 
+    //show user options for the corpus
+    printf("Please type a letter from A to K as two which text you would like (has to be capitals):\n");
+
+    int letterCheck = 0;
+    char ActionChoice1[128];
+    while(letterCheck == 0)
+    {
+        //user input (just to confirm that the user can return back to the previous menu)
+        printf(": ");
+        scanf("%s", &ActionChoice1);
+
+        if(strlen(ActionChoice1) == 1)
+        {
+            int charToInt = (int)ActionChoice1[0];
+            if (charToInt >= 65 && charToInt <= 75)
+            {
+                break;
+            }
+            else
+            {
+                printf("please input a letter inside the required range and is capitalised\n");
+            }
+        }
+        else
+        {
+            printf("your choice needs to be one letter\n");
+        }
+    }
+
     printf("***Generating list***\n");
 
-    FILE* fp;
-    char nextString[2048];
+    FILE* Wfp;
 
-    fp = fopen("./corpus/Texts/A/A0/A00.xml", "r");
+    //this is the file where all the words are deposited
+    Wfp = fopen("./corpus/words.txt", "w");
+
+    if (Wfp == NULL)
+    {
+        printf("Error: unable to open word file\n");
+        return -1;
+    }
+
+    struct dirent *dp;
+    char directoryName[128] = "./corpus/Texts/";
+    strcat(directoryName, ActionChoice1);
+    //printf("%s\n", directoryName);
+
+    DIR *dir = opendir(directoryName);
+    if (!dir)
+    {
+        printf("Error: directory doesn't exist\n");
+        return -1;
+    }
+    else
+    {
+        while ((dp = readdir(dir)) != NULL)
+        {
+            if (!(!strcmp(dp->d_name,"..") || !strcmp(dp->d_name, ".")))
+            {
+                char innnerDirectoryName[128];
+                strcpy(innnerDirectoryName, directoryName);
+                strcat(innnerDirectoryName, "/");
+                strcat(innnerDirectoryName, dp->d_name);
+                //printf("%s\n", innnerDirectoryName);
+
+                struct dirent *innerdp;
+                DIR *innerdir = opendir(innnerDirectoryName);
+                if(!innerdir)
+                {
+                    printf("Error: directory doesn't exist\n");
+                    return -1;
+                }
+                else
+                {
+                    while((innerdp = readdir(innerdir)) != NULL)
+                    {
+                        if (!(!strcmp(innerdp->d_name,"..") || !strcmp(innerdp->d_name, ".")))
+                        {
+                            char FinalDirectoryName[128];
+                            strcpy(FinalDirectoryName, innnerDirectoryName);
+                            strcat(FinalDirectoryName, "/");
+                            strcat(FinalDirectoryName, innerdp->d_name);
+                            printf("%s\n", FinalDirectoryName);
+                        }
+                    }
+                    closedir(innerdir);
+                }
+            }
+        }
+        closedir(dir);
+    }
+
+    
+    fclose(Wfp);
+
+    //confirm that the file was generated successfully
+    printf("***File generated sucessfully***\n");
+
+    //user input (just to confirm that the user can return back to the previous menu)
+    char ActionChoice2[128];
+    printf("Type anything to return: ");
+    scanf("%s", &ActionChoice2);
+
+    return 0;
+}
+
+int wordReader(char fileLocation[128], FILE* Wfp)
+{
+    FILE* fp;
+    char nextChar;
+
+    fp = fopen(fileLocation, "r");
 
     if (fp == NULL)
     {
@@ -182,23 +291,66 @@ int LWD_Dataset()
     }
     else
     {
-        while(!feof(fp))
+        //line count just used for testing purposes
+        int lineCount = 1;
+
+        while(nextChar != EOF)
         {
-            fgets(nextString, 2047, fp);
-            printf("%s", nextString);
+            nextChar = fgetc(fp);
+            if (nextChar == '\n') lineCount++;
+            //the format of a word in the XML file is:
+            //<w ... hw="word" ...
+            //so the following code performs the checks to extract the word from the quotation marks
+            else if(nextChar == '<')
+            {
+                nextChar = fgetc(fp);
+                if (nextChar == 'w')
+                {
+                    nextChar = fgetc(fp);
+                    if (nextChar == ' ')
+                    {
+                        char nextString[128];
+
+                        while(nextChar != 'h')
+                        {
+                            nextChar = fgetc(fp);
+                        }
+                        for (int i=0; i<4; i++)
+                        {
+                            nextChar = fgetc(fp);
+                        }
+
+                        //check to see if a digit was found
+                        int breakCheck = 0;
+
+                        int charCount = 0;
+                        while(nextChar != '"')
+                        {
+                            if (isdigit(nextChar))
+                            {
+                                breakCheck++;
+                                break;
+                            }
+                            nextString[charCount] = nextChar;
+                            nextChar = fgetc(fp);
+                            charCount++;
+                        }
+                        nextString[charCount] = '\0';
+                        //word shouldn't have numbers or start on anything other than a letter
+                        //if a word is valid, it is added to the final file of words
+                        if (breakCheck == 0 && isalpha(nextString[0]))
+                        {
+                            fprintf(Wfp, "%s\n", nextString);
+                            //printf("%d: %s\n", lineCount, nextString);
+                        }
+                    }
+                }
+            }
         }
+        //printf("%d\n", lineCount);
     }
 
     //cleanup
     fclose(fp);
 
-    //confirm that the file was generated successfully
-    printf("***File generated sucessfully***\n");
-
-    //user input (just to confirm that the user can return back to the previous menu)
-    char ActionChoice[128];
-    printf("Type anything to return: ");
-    scanf("%s", &ActionChoice);
-
-    return 0;
 }
